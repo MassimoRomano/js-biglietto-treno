00414fd1b76b591e93c1835fb864079c7d381c6a
MassimoRomano <romano93m@gmail.com>
2024-02-19 17:55:34 +0100

    fix error string

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 const forKm = 0.21
 // add price for km 
 
-const travelerKm = Number(prompt(How many km do you have to travel))
+const travelerKm = Number(prompt("How many km do you want to travel for?"))
 // Site request to the customer for the kilometers to travel
 
-const travelerAge = Number(prompt(How old are you?))
+const travelerAge = Number(prompt("How old are you?"))
 //Site request to the customer for the age
