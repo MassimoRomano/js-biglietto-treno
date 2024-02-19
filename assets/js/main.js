44115ab642a928bf24fabcd619dff4fae86cad58
MassimoRomano <romano93m@gmail.com>
2024-02-19 19:11:00 +0100

    fix discount

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,20 +10,22 @@ const travelerAge = Number(prompt("How old are you?"))
 const travelerKm = Number(prompt("How many km do you want to travel for?"))
 // Site request to the customer for the kilometers to travel
 
-const discountMin = 0.2
-//discount del 20%
-const discountMax = 0.4
-//discount del 40%
 const fullPrice = forKm * travelerKm
 //ful price
 
+const discountMin = fullPrice * 0.2
+//discount del 20%
+const discountMax = fullPrice * 0.4
+//discount del 40%
+
+
 if(travelerAge < 18){
-    price = (fullPrice - (forKm * discountMin)).toFixed(2)
+    price = (fullPrice - (discountMin)).toFixed(2)
 }
 //price for <18
 
 else if (travelerAge => 65){
-    price = (fullPrice - (forKm * discountMax)).toFixed(2)
+    price = (fullPrice - (discountMax)).toFixed(2)
 }
 //price for =>65
 
@@ -32,4 +34,5 @@ else{
 }
 // full price
 
-
+alert(`price: ${price} €`);
+console.log(`price: ${price} €`);
