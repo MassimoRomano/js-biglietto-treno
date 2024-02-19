361a2e213ad01280a636815f525b902422ef4120
MassimoRomano <romano93m@gmail.com>
2024-02-19 17:53:53 +0100

    add Step 1 step 2 and step 3

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const forKm = 0.21
+// add price for km 
+
+const travelerKm = Number(prompt(How many km do you have to travel))
+// Site request to the customer for the kilometers to travel
+
+const travelerAge = Number(prompt(How old are you?))
+//Site request to the customer for the age
